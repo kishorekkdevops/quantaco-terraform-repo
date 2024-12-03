2bf8b5b1263f48641f688741a3dff8d6dada2c4f
kishore <kishore@infyrabo.onmicrosoft.com>
2024-12-03 11:01:19 +0530

    all updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "gcs" {
     bucket = "quantacobucket"
     prefix = "terraform/global"
-    #project = "quantaco-443301"
-    credentials = "./quantaco-443301-90aa0b1191d8.json"
+    //project = "quantaco-443301"
+    //credentials = "./quantaco-443301-90aa0b1191d8.json"
   }
 }
