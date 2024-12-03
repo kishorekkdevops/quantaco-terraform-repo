bab2a62790d26f85e5f00232ea283f473690c840
kishore <kishore@infyrabo.onmicrosoft.com>
2024-12-03 10:39:48 +0530

    second commit

--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
 variable "credentials_file" {
   description = "Path to the GCP credentials file"
   type        = string
-  //default     = "./nichecloudexpert-1bbb33e34e42.json"
+  //default     = "./quantaco-443301-90aa0b1191d8.json"
 }
 
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default     = "nichecloudexpert"
+  default     = "quantaco-443301"
 }
 
 variable "region" {
